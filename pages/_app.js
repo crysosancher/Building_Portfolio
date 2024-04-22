93f73444553619269ea53196473a237bedbb1151
crysosancher <vibhupandey.vp@gmail.com>
2024-04-23 00:03:22 +0530

    Seo

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,35 @@ function MyApp({ Component, pageProps }) {
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-          rel="stylesheet"
+          rel="stylesheet"/>
+           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="I'm a Developer I Can Develop Anything You Want"
+        />
+        <meta
+          name="keywords"
+          content="vibhu Pandey,vibhu,pandey,vibhu portfolio, vibhu cryso,cryso, cryso portfolio, cryso portfolio website, cryso website, cryso portfolio website, cryso portfolio, cryso media, cryso media website, cryso media portfolio, cryso media portfolio website, cryso media website, cryso media portfolio website, cryso media portfolio, cryso media, cryso media website, cryso media portfolio, cryso media portfolio website, cryso media website, cryso media portfolio website, cryso media portfolio, cryso media, cryso media website"
+        />
+        <meta
+          property="og:image"
+          content="Vibhu Pandey"
+        />
+        <meta property="og:title" content="Vibhu Pandey" />
+        <meta
+          property="og:description"
+          content="As a developer, I'm all about making your dreams click! I can whip up anything you dream of into digital reality. From sleek websites to nifty apps, I've got the knack to turn your ideas into code magic. Think it, and I'll make it happen – that's my specialty. Let's chat about your vision and watch it come alive on screen!"
+        />
+        <meta property="og:URL" content="vibhu " />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="index, follow" />
+        <link rel="shortcut icon" href="/favicons/favicons.ico" />
+        <meta
+          name="google-site-verification"
+          content="sJN2MPzgDpzPOlooLsyGEpQUDoaPdXM9OYMBoCGuzHU"
+        
         />
       </Head>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -21,34 +21,6 @@ const Index = () => {
     <Layout>
       <Head>
       {/* <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" /> */}
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="Are you looking for a one-stop destination to access the latest and greatest content, for FREE? You've come to the right place! Join handpicked WhatsApp groups and Telegram channels where you can network, share insights, and stay updated on industry trends."
-        />
-        <meta
-          name="keywords"
-          content="Shiva,Shiva family,Shiva birthday,stats,donate,gdrive,Shiva website,Shiva webpage,Shiva community,Shiva bot,whatsapp group,whatsapp bot,insta downloader,youtube downloader,movies,series,technology,news,programming,crypto,stocks,anime,sports,sticker,bot,study,meme,gaming,automobile,astronomy,space,bgmi,coc,status,Shiva whatsapp group,Shiva telegram group,Shiva telegram channel,Shiva facebook group,Shiva discord channel"
-        />
-        <meta
-          property="og:image"
-          content="Shiva"
-        />
-        <meta property="og:title" content="Shiva" />
-        <meta
-          property="og:description"
-          content="Are you looking for a one-stop destination to access the latest and greatest content, for FREE? You've come to the right place! Join handpicked WhatsApp groups and Telegram channels where you can network, share insights, and stay updated on industry trends."
-        />
-        <meta property="og:URL" content="Shiva" />
-        <meta property="og:type" content="website" />
-        <meta name="robots" content="index, follow" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta
-          name="google-site-verification"
-          content="sJN2MPzgDpzPOlooLsyGEpQUDoaPdXM9OYMBoCGuzHU"
-        />
         <title>Cryso | Portfolio</title>
       </Head>
       <Home />
